a05b52b11ffb8e857ed41e5daafc42012e000af1
Emilia Piechocka <couchcodergirl@gmail.com>
2020-06-25 15:23:16 +0200

    Add initAmountWidget in CartProducts class

--- src/js/script.js
+++ src/js/script.js
@@ -333,21 +333,20 @@
 
   }
 
-  class CartProduct{
-    constructor(menuProduct, element){
+  class CartProduct {
+    constructor(menuProduct, element) {
       const thisCartProduct = this;
-
+  
       thisCartProduct.id = menuProduct.id;
       thisCartProduct.name = menuProduct.name;
       thisCartProduct.price = menuProduct.price;
       thisCartProduct.priceSingle = menuProduct.priceSingle;
       thisCartProduct.amount = menuProduct.amount;
-      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));      
-
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+  
       thisCartProduct.getElements(element);
-
-      console.log('newCartProduct', thisCartProduct);
-      console.log('productData', menuProduct);
+      thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
     }
 
     getElements(element){
@@ -356,12 +355,25 @@
       thisCartProduct.dom = {};
 
       thisCartProduct.dom.wrapper = element;
+      
       thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
       thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
       thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
       thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
     }
-    
+    initAmountWidget() {
+      const thisCartProduct = this;
+  
+      thisCartProduct.amountWidget = new AmountWidget(
+        thisCartProduct.dom.amountWidget
+      );
+      thisCartProduct.dom.amountWidget.addEventListener('updated', function() {
+        thisCartProduct.amount = thisCartProduct.amountWidget.value;
+        thisCartProduct.price =
+          thisCartProduct.priceSingle * thisCartProduct.amount;
+        thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
   }
 
   const app = {
